a09ab4575044f17c2061a59270e7e1974438d553
dian candra pribadi <diancandra112@gmail.com>
2019-09-18 21:15:41 +0700

    prifile

--- src/screens/HomeMitra.js
+++ src/screens/HomeMitra.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-
+import { View, Text, StyleSheet, Image } from 'react-native'
+// import { Image } from "native-base";
 class HomeMitra extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Home Mitra</Text>
-            </View>
+            // <View style={styles.container}>
+                <Image style={{ height:200, width:200, resizeMode:'stretch'}} source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTWMTKBph4g3sCBqyDlWa68iLUGdKjylvGp41d0PBnB4YGerToj'}} ></Image>
+            // </View>
         )
     }
 }
@@ -16,9 +16,5 @@ export default HomeMitra
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'green',
-        flex: 1
     }
 })
--- src/screens/ProfileMitra.js
+++ src/screens/ProfileMitra.js
@@ -1,11 +1,61 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-
+import { View, Text, StyleSheet, Dimensions, Image, ScrollView } from 'react-native'
+const { height, width } = Dimensions.get('window')
 class ProfileMitra extends React.Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text>Profile Mitra</Text>
+            <View style={{flex:1}}>
+                <View style={{backgroundColor:'red', borderWidth:1, borderStyle:'solid', height:height/2.5, width:width, borderBottomStartRadius:20, borderBottomEndRadius:20}}>
+                    <Image source={{uri:'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRMJKd1IfYPwx8sGUXnnItiJ4SePsmShD-2Bh3gVBydbpBcRuPy'}} style={{ height:200, width:200, resizeMode:'stretch'}}/>
+                </View>
+                <View 
+                style={{backgroundColor:'green',borderWidth:1, borderStyle:'solid', 
+                height:width/2, width:width/1.3, position:'absolute',borderRadius:10, 
+                alignSelf:'center',top:'30%', padding:10,}}>
+
+                    <View style={{ height:'35%', width:'45%', alignSelf:'center', marginTop:10, borderRadius:10}}>
+                        <Image style={{backgroundColor:'red',height:'100%', width:'100%', borderRadius:10}}/>
+                    </View>
+                    <Text style={{alignSelf:'center', fontSize:22, fontFamily:'Roboto', marginTop:10}}>NTech Malang</Text>
+                    <Text style={{alignSelf:'center', fontSize:15, fontFamily:'Roboto', marginTop:5}}>Hotel</Text>
+
+                        <Image source={require('../assets/images/edit.png')} style={{ height:50, width:50, resizeMode:'stretch', position:'absolute', bottom:'5%', left:'5%'}}/>
+                       
+
+                </View>
+
+                <View
+                style={{height:width/2, width:width,
+                alignSelf:'center',top:'20%'}}>
+                <ScrollView>
+                    <Text style={{alignSelf:'flex-start', fontSize:22, fontFamily:'Roboto', marginTop:5, marginLeft:10}}>About Me</Text>
+                    
+                    <View style={{borderStyle:'dashed', borderBottomWidth:0.2, height:75, flexDirection:'row', marginHorizontal:10}}>
+                        <Image style={{backgroundColor:'red', height:50, width:50, marginVertical:10, marginRight:10}}/>
+                        <View style={{marginLeft:10}}>
+                            <Text style={{fontSize:20, marginTop:5, fontFamily:'Roboto', fontWeight:'500'}}>Owner Name</Text>
+                            <Text style={{marginTop:5, fontWeight:'100'}}>diancandra112@gmail.com</Text>
+                        </View>
+                    </View>
+
+                    <View style={{borderStyle:'dashed', borderBottomWidth:0.2, height:75, flexDirection:'row', marginHorizontal:10}}>
+                        <Image style={{backgroundColor:'red', height:50, width:50, marginVertical:10, marginRight:10}}/>
+                        <View style={{marginLeft:10}}>
+                            <Text style={{fontSize:20, marginTop:5, fontFamily:'Roboto', fontWeight:'500'}}>GMAIL</Text>
+                            <Text style={{marginTop:5, fontWeight:'100'}}>diancandra112@gmail.com</Text>
+                        </View>
+                    </View>
+                    
+                    <View style={{borderStyle:'dashed', borderBottomWidth:0.2, flexDirection:'row', marginHorizontal:10}}>
+                        <Image style={{backgroundColor:'red', height:50, width:50, marginVertical:10, marginRight:10}}/>
+                        <View style={{marginLeft:10}}>
+                            <Text style={{fontSize:20, marginTop:5, fontFamily:'Roboto', fontWeight:'500'}}>ADDRESS</Text>
+                            <Text style={{marginTop:5, fontWeight:'100'}}>diancandra112@gmail.com aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</Text>
+                        </View>
+                    </View>
+
+                </ScrollView>
+                </View>
             </View>
         )
     }
@@ -13,12 +63,3 @@ class ProfileMitra extends React.Component {
 
 export default ProfileMitra
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: 'green',
-        flex: 1
-    }
-})
