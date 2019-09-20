87b3164c063e7f8ae3b1ce6fd7c8ac1e27002182
Fuad Mustamirrul Ishlah 83 <amir24405@gmail.com>
2019-09-21 02:04:29 +0700

    Edit mitra functionality

--- src/screens/EditProfileMitra.js
+++ src/screens/EditProfileMitra.js
@@ -1,23 +1,42 @@
 import React from 'react'
-import {View, Dimensions, TextInput, Image, TouchableOpacity, ScrollView, Text} from 'react-native'
+import {View, Dimensions, TextInput, Image, TouchableOpacity, ScrollView, Text, StyleSheet} from 'react-native'
 import ImagePicker from 'react-native-image-picker';
 import firebase from 'react-native-firebase'
 import uuid from 'uuid/v4'; 
+import MapView, { Marker } from 'react-native-maps';
+import AsyncStorage from '@react-native-community/async-storage'
+import Axios from 'axios';
+import config from '../config/config';
 
 const {width,height} = Dimensions.get('window')
 export default class EditProfileMitra extends React.Component{
 
     constructor(props) {
-        super(props);
-            this.state = {
-                avatarSource: null,
-                imageUrl:'',
-                imageUri:'',
-                uploading: false,
-                progress: 0,
-            }
-        this.selectPhotoTapped = this.selectPhotoTapped.bind(this)
-        this.uploadImage = this.uploadImage.bind(this)
+      super(props);
+      const profile = props.navigation.getParam('profile')
+      this.state = {
+          avatarSource: null,
+          imageUrl:'',
+          imageUri:'',
+          uploading: false,
+          progress: 0,
+          profile,
+          formData:{
+            id: profile.id,
+            name: profile.name,
+            company: profile.company,
+            image:profile.image
+          }
+      }
+      console.warn(props.navigation.getParam('profile'))
+      this.selectPhotoTapped = this.selectPhotoTapped.bind(this)
+      this.uploadImage = this.uploadImage.bind(this)
+    }
+
+    handleChange = (name, text) =>{
+      let newFormData = this.state.formData
+      newFormData[name] = text
+      this.setState({formData:newFormData})
     }
 
     selectPhotoTapped() {
@@ -54,45 +73,69 @@ export default class EditProfileMitra extends React.Component{
       }
     
       uploadImage = () => {
-        console.log('haiiii')
-        
-        const ext = this.state.imageUri.split('.').pop(); // Extract image extension
-        const filename = `${uuid()}.${ext}`; // Generate unique name
-        this.setState({ uploading: true });
-        firebase
-          .storage()
-          .ref(`tutorials/images/${filename}`)
-          .putFile(this.state.imageUri)
-          .on(
-            firebase.storage.TaskEvent.STATE_CHANGED,
-            snapshot => {
-              let state = {};
-              state = {
-                ...state,
-                progress: (snapshot.bytesTransferred / snapshot.totalBytes) * 100 // Calculate progress percentage
-              };
-              if (snapshot.state === firebase.storage.TaskState.SUCCESS) {
-                
+        if(this.state.imageUri === ''){
+          this.editProfile()
+        }else{
+          const ext = this.state.imageUri.split('.').pop(); // Extract image extension
+          const filename = `${uuid()}.${ext}`; // Generate unique name
+          this.setState({ uploading: true });
+          return firebase
+            .storage()
+            .ref(`tutorials/images/${filename}`)
+            .putFile(this.state.imageUri)
+            .on(
+              firebase.storage.TaskEvent.STATE_CHANGED,
+              snapshot => {
+                let state = {};
                 state = {
                   ...state,
-                  uploading: false,
-                  imageUri: '',
-                  imageUrl:snapshot.downloadURL,
-                  progress: 0,
+                  progress: (snapshot.bytesTransferred / snapshot.totalBytes) * 100 // Calculate progress percentage
                 };
+                if (snapshot.state === firebase.storage.TaskState.SUCCESS) {
+                  
+                  state = {
+                    ...state,
+                    uploading: false,
+                    imageUri: '',
+                    imageUrl:snapshot.downloadURL,
+                    progress: 0,
+                  };
+                }
+                this.setState(state,this.editProfile());
+                
+              },
+              error => {
+                unsubscribe();
+                alert('Sorry, Try again.');
               }
-              this.setState(state);
-              console.log(this.state.imageUrl);
-              
-            },
-            error => {
-              unsubscribe();
-              alert('Sorry, Try again.');
-            }
-          );
+            );
+        }
     };
+    editProfile = () => {
+      let data = this.state.formData
+      data.image = this.state.imageUrl
+      Axios.patch(config.host +'/edit/partner',data,{
+        headers:{
+          Authorization: this.state.token
+        }
+      })
+      .then(()=>{
+        this.props.navigation.goBack()
+      })
+      .catch(console.log)
+    }
+    componentDidMount(){
+      AsyncStorage.getItem('token',async (err, result)=>{
+        if (!err) {
+            this.setState({token:result})
+        } else {
+            console.warn(err)
+        }
+    })
+    }
 
     render(){
+      const {profile} = this.state
         return(
             <View style={{flex:1}}>
                 <ScrollView>
@@ -109,25 +152,16 @@ export default class EditProfileMitra extends React.Component{
                     </View>
 
                     <View style={{borderBottomWidth:0.8, borderStyle:'solid', marginHorizontal:10, marginTop:10}}>
-                        <TextInput placeholder='Owner Name'/>
+                        <TextInput onChangeText={(text) => this.handleChange('name',text)} placeholder={profile.name}/>
                     </View>
 
                     <View style={{borderBottomWidth:0.8, borderStyle:'solid', marginHorizontal:10, marginTop:10}}>
-                        <TextInput placeholder='Company Name'/>
+                        <TextInput onChangeText={(text) => this.handleChange('company',text)} placeholder={profile.company}/>
                     </View>
 
-                    <View style={{borderBottomWidth:0.8, borderStyle:'solid', marginHorizontal:10, marginTop:10}}>
-                        <TextInput placeholder='Email'/>
-                    </View>
-
-                    
-                    <View style={{borderBottomWidth:0.8, borderStyle:'solid', marginHorizontal:10, marginTop:10}}>
-                        <TextInput placeholder='Latitude'/>
-                    </View>
-
-                    <View style={{borderBottomWidth:0.8, borderStyle:'solid', marginHorizontal:10, marginTop:10}}>
-                        <TextInput placeholder='Longitude'/>
-                    </View>
+                    {/* <View style={{borderBottomWidth:0.8, borderStyle:'solid', marginHorizontal:10, marginTop:10}}>
+                        <TextInput onChangeText={(text) => this.handleChange('email',text)} placeholder={profile.email}/>
+                    </View> */}
 
                     <TouchableOpacity onPress={this.uploadImage.bind(this)} 
                         style={{ backgroundColor: '#66a1e7', 
