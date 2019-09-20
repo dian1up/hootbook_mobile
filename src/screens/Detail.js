4a33e1ce79abfcca006f8d88d08c59e726f12ebe
Fuad Mustamirrul Ishlah 83 <amir24405@gmail.com>
2019-09-20 14:36:12 +0700

    detail

--- src/screens/Detail.js
+++ src/screens/Detail.js
@@ -6,6 +6,7 @@ import MapView, { Marker } from 'react-native-maps'
 import DatePicker from 'react-native-datepicker'
 import AsyncStorage from '@react-native-community/async-storage';
 import config from '../config/config';
+import Axios from 'axios';
 import JsonWebToken from "react-native-pure-jwt";
 
 const { width, height } = Dimensions.get('window')
@@ -26,24 +27,24 @@ class Detail extends React.Component {
                 latitudeDelta: 0.00922 * 1.5,
                 longitudeDelta: 0.00421 * 1.5
             },
-            date: new Date(),
             currentUserData:{},
         };
     }
     decodeJwt = (inputJwt) => {
       return JsonWebToken.decode(
-          inputJwt, // the token
-          config.secret, // the secret
+          inputJwt, 
+          config.secret, 
           {
-            skipValidation: true // to skip signature and exp verification
+            skipValidation: true 
           }
         )
-        .then(res => res) // already an object. read below, exp key note
+        .then(res => res) 
         .catch(console.error);
     }
     componentDidMount(){
       AsyncStorage.getItem('token',async (err, result)=>{
         if (!err) {
+            this.setState({token:result})
             let decode = await this.decodeJwt(result.split(' ')[1])
             console.log('data = ', decode.payload)
             this.setState({currentUserData:decode.payload})
@@ -52,6 +53,37 @@ class Detail extends React.Component {
             console.warn(err)
         }
       })
+    }
+    onPressBooking = () => { 
+        const { data,check_in,check_out, token } = this.state
+        if(check_out){
+            let check_out_arr = check_out.split('-')
+            let check_in_arr = check_in.split('-')
+            const checkOut  = new Date(check_out_arr[2], check_out_arr[1], check_out_arr[0])
+            const checkIn  = new Date(check_in_arr[2], check_in_arr[1], check_in_arr[0])
+            console.log(checkOut, check_out)
+            const bookingData = {
+                service_id: data.id,
+                check_in:checkIn,
+                check_out:checkOut
+            }
+            console.log(bookingData)
+            Axios.post(`${config.host}/booking`, bookingData, {
+                headers:{
+                    Authorization: token
+                }
+            })
+            .then(res => {
+                console.log(res)
+                alert('Success')
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }else{
+            alert('Please select checkout date')
+        }
+
     }
     onPressMessage = () => {
         const { data, currentUserData } = this.state
@@ -107,16 +139,15 @@ class Detail extends React.Component {
                                 <View>
                                     <Image source={require('../assets/Icons/marker1.png')} />
                                 </View>
-
                             </Marker>
                         </MapView>
                     </View>
                     <View style={{ flex: 2, flexDirection: 'row', justifyContent: 'flex-end', alignItems: 'center' }}>
                         <DatePicker
                             style={{ width: '50%', justifyContent: 'flex-start' }}
-                            date={this.state.date} //initial date from state
                             mode="date" //The enum of date, datetime and time
                             placeholder="Check in"
+                            date={this.state.check_in}
                             format="DD-MM-YYYY"
                             minDate={new Date()}
                             // maxDate="01-01-2019"
@@ -136,11 +167,11 @@ class Detail extends React.Component {
                                     paddingLeft: 7
                                 }
                             }}
-                            onDateChange={(date) => { this.setState({ date: date }) }}
+                            onDateChange={(date) => { this.setState({ check_in: date }) }}
                         />
                         <DatePicker
                             style={{ width: '50%' }}
-                            date={this.state.date} //initial date from state
+                            date={this.state.check_out || new Date()} //initial date from state
                             mode="date" //The enum of date, datetime and time
                             placeholder="Check out"
                             format="DD-MM-YYYY"
@@ -160,7 +191,7 @@ class Detail extends React.Component {
                                     paddingLeft: 7
                                 }
                             }}
-                            onDateChange={(date) => { this.setState({ date: date }) }}
+                            onDateChange={(date) => { this.setState({ check_out: date }) }}
                         />
                     </View>
 
@@ -195,8 +226,6 @@ class Detail extends React.Component {
                             (<Text>Type: <Image
                                 source={require('../assets/Icons/single-bed.png')}
                             /><Text> 2 orang/kamar</Text></Text>)}
-
-
                     </View>
                     <View style={{ paddingBottom: 30 }}>
                         <Text style={{ fontSize: 15, fontWeight: '700' }}>Description</Text>
@@ -206,7 +235,7 @@ class Detail extends React.Component {
                 </ScrollView>
                 <View style={{ height: 50, flexDirection: "row", }}>
                     <View style={{ backgroundColor: '#295989', flex: 2 }}>
-                        <TouchableOpacity style={{ width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+                        <TouchableOpacity onPress={this.onPressBooking} style={{ width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
                             <Text style={{ color: '#fff', fontSize: 18, fontWeight: '700' }}>Booking Now</Text>
                         </TouchableOpacity>
                     </View>
